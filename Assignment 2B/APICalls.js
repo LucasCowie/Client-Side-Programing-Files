a1f3202395db23c77b300a6a34066024701e661e
Lucas <w0487255@nscc.ca>
2026-02-03 14:57:52 -0400

    Added monster hunter api that has no CORS

--- Assignment 2B/APICalls.js
+++ Assignment 2B/APICalls.js
@@ -1,14 +1,41 @@
 function getDate(){
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php?a=Alcoholic")
     .then(respone => respone.json())
     .then(data => {
         console.log(data.drinks);
+        const drink = data.drinks[0];
+ 
         const drinkName = document.getElementById("drinkName");
         const drinkPic = document.getElementById("drinkPic");
-        const name = data.drinks[0].strDrink;
-        const url = data.drinks[0].strDrinkThumb;
+        const drinkType = document.getElementById("drinkType");
+
+
+        const name = drink.strDrink;
+        const url = drink.strDrinkThumb;
+        const type = drink.strAlcoholic;
+
         drinkName.textContent = name;
         drinkPic.innerHTML = "<img src="+url+">"
-    });
+        drinkType.textContent = type;
+
+
+    }).catch(err => console.error(err));
 }
 getDate();
+function getDateDino(){
+    var num = Math.floor(Math.random() * (59 - 17)) + 17;
+    fetch("https://mhw-db.com/monsters/"+num)
+    .then(respone => respone.json())
+    .then(data => {
+        console.log(data);
+        const dino = data;
+ 
+        const dinoName = document.getElementById("dinoName")
+
+        const name = dino.name;
+
+        dinoName.textContent = name;
+
+    }).catch(err => console.error(err));
+}
+getDateDino();
